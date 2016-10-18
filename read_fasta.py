fe8adcc2c7cd634ea2dd5b29a091608656a38b0e
Rui Dai <rui.dai.mail@gmail.com>
2016-10-18 12:08:18 -0400

    prints argument filename first

--- read_fasta.py
+++ read_fasta.py
@@ -17,7 +17,7 @@ if len(sys.argv) < 2:
     print "Usage:", sys.argv[0], "<sequence.fa>"
     exit(1)
 
-print read_fasta(sys.argv[1])
+print sys.argv [1], ';', read_fasta(sys.argv[1])
 
 ## that's all folks
 
